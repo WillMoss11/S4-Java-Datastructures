24fad6b5361d64a58670943e4ca9189ad59fcfd0
WillMoss11 <william.moss@keyin.com>
2025-01-20 23:35:20 -0330

    Update

--- src/Assignments/Temperature.java
+++ src/Assignments/Temperature.java
@@ -1,7 +1,7 @@
 package Assignments;
 
 /**
- * Calculate the average temperature, and give how many temperatures are above the average temperature
+ * Calculate the average temperature, and give how many days are above the average temperature
   1. Take an input from the user (eg. 5)
   2. Prompt the user to enter all the 5 numbers (temperature values)
   3. Calculate the average temperature
