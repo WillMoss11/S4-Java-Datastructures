273b03d410e7f2f9ae3a5e75ef801c87784614a6
WillMoss11 <william.moss@keyin.com>
2025-01-22 23:28:31 -0330

    Update

--- src/Assignments/MovieTheater.java
+++ src/Assignments/MovieTheater.java
@@ -8,4 +8,21 @@ package Assignments;
 
 
 public class MovieTheater {
+    private char[][] seats;
+
+    public MovieTheater(int rows, int cols) {
+        seats = new char[rows][cols];
+        initializeTheater();
+    }
+
+    // Initialize all seats to A
+    private void initializeTheater() {
+        for (int i = 0; i < seats.length; i++) {
+            for (int j = 0; j < seats[i].length; j++) {
+                seats[i][j] = 'A';
+            }
+        }
+    }
+
+    // Reserve a Seat
 }
