869fcee0d8dd7f869a1bac5d57ed2bcc6984935a
WillMoss11 <william.moss@keyin.com>
2025-02-03 18:23:39 -0330

    Changes

--- src/Assignments/LinkedList/Node.java
+++ src/Assignments/LinkedList/Node.java
@@ -1,8 +1,13 @@
 package Assignments.LinkedList;
 
 public class Node {
-    public int value;
+    public int data;
     public Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
 
